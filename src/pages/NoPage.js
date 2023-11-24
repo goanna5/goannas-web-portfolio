9f5cb31f536e2f513acd93e261860e093ba118fd
goanna5 <143087684+goanna5@users.noreply.github.com>
2023-11-24 15:17:19 +1000

    404 error page

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -2,8 +2,12 @@ import '../App.css';
 const NoPage = () => {
     return (
     <>
+    <div className="centre-div">
     <h1>404</h1>
-    <p>Page is not found</p>
+    <p>Can't find the page you're looking for</p><br></br>
+    <p>And goanna is sad</p>
+    <img src="../sad_goanna.gif" alt="A sad goanna" />
+    </div>
     </>
     )
   };
