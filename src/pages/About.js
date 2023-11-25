1413f8e9919edacc2d34d4892810f44e123fade1
goanna5 <143087684+goanna5@users.noreply.github.com>
2023-11-25 18:28:41 +1000

    added about me text- needs formatting to look cool

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,28 @@
 import '../App.css';
 const About = () => {
-    return <h1>About Me</h1>;
+    return (
+    <>
+    <div className="centre-div">
+    <h1>About Me</h1>
+    <br></br>
+    <p>Hi I'm Anna although on all my profiles I go by Goanna. I'm heading into my second year
+      of the integrated Bachelor of Computer Science and Master of Cyber Security at UQ, majoring in 
+      Cyber Security.
+    </p>
+    <p>
+    Whilst I am interested in the Cyber Security environment, I also have an interest in software development so
+    I'm looking to further my general programming skills through a series of projects:
+    </p>
+    <ul>
+      <li>this website, learning front-end web development</li>
+      <li>a discord bot to play around with certain commands and possible API calls</li>
+      <li>a minecraft mod because I enjoy playing, and I'm wishing to consolidate my Java knowledge</li>
+      <li>and potentially more in the future</li>
+    </ul>
+    </div>
+    </>
+    )
+
   };
   
   export default About;
